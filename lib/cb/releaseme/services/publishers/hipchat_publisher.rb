586ee1bcc4d16b200abfdc1fb137d0471b2e7c4e
Jay Danielian <jay.danielian@circleback.com>
2016-08-25 12:11:00 -0400

    namespacing is a bitch

--- lib/cb/releaseme/services/publishers/hipchat_publisher.rb
+++ lib/cb/releaseme/services/publishers/hipchat_publisher.rb
@@ -1,34 +1,36 @@
 require 'hipchat'
 
-module Services
-  module Publishers
+module ReleaseMe
+  module Services
+    module Publishers
 
-    class HipChatPublisher
+      class HipChatPublisher
 
-      def initialize(api_token)
+        def initialize(api_token)
 
 
-        @client = HipChat::Client.new(api_token)
+          @client = HipChat::Client.new(api_token)
 
+
+
+        end
+
+        def publish_release(release_version, system_name, env, room_name, issues = [])
+
+
+          output = ''
+          issues.each{|i| output << "<a href=\"#{i.link}\">#{i.id}</a> - #{i.title}<br/>"  }
+
+          message = "#{system_name} version #{release_version} released to #{env.to_s.upcase}<br/> #{output}"
+
+          @client[room_name].send('cap deploy', message, :message_format => 'html')
+
+
+        end
 
 
       end
-
-      def publish_release(release_version, system_name, env, room_name, issues = [])
-
-
-        output = ''
-        issues.each{|i| output << "<a href=\"#{i.link}\">#{i.id}</a> - #{i.title}<br/>"  }
-
-        message = "#{system_name} version #{release_version} released to #{env.to_s.upcase}<br/> #{output}"
-
-        @client[room_name].send('cap deploy', message, :message_format => 'html')
-
-
-      end
-
 
     end
-
   end
 end
--- lib/cb/releaseme/services/source_managers/git_manager.rb
+++ lib/cb/releaseme/services/source_managers/git_manager.rb
@@ -1,80 +1,82 @@
 require 'git'
 require 'logger'
 
-module Services
-  module SourceManagers
+module ReleaseMe
+  module Services
+    module SourceManagers
 
-    class GitManager
+      class GitManager
 
-      def initialize(working_path)
+        def initialize(working_path)
 
-        if Dir.exists?(working_path)
-          @git = Git.open(working_path, :log => Logger.new(STDOUT))
-        else
-          raise ArgumentError.new("working_path '#{working_path}' not found")
-        end
-
-      end
-
-      def get_commits(prev_tag, current_tag)
-
-        git_commits = @git.log.between(prev_tag, current_tag)
-
-        mapped_commits = git_commits.map do |gc|
-
-          c = Commit.new
-          c.author = gc.author
-          c.message = gc.message
-          c.name = gc.name
-          c.committed_on = gc.date
-
-          c
-
-        end
-
-        mapped_commits
-
-      end
-
-      def tag_exists(tag_name)
-        exists = false
-        begin
-          @git.tag(tag_name)
-            exists = true
-        rescue Git::GitTagNameDoesNotExist => tag_error
-
-        end
-
-        exists
-
-      end
-
-      def get_story_ids(commits, story_id_regex_pattern = '^\[?([A-Z]{2,8}-\d{1,11})')
-
-        ids = []
-        re = Regexp.new(story_id_regex_pattern)
-
-        commits.each do |commit|
-          re.match(commit.message) do |m|
-            ids << m[1] if m.length > 0
+          if Dir.exists?(working_path)
+            @git = Git.open(working_path, :log => Logger.new(STDOUT))
+          else
+            raise ArgumentError.new("working_path '#{working_path}' not found")
           end
+
         end
 
-        ids.uniq
+        def get_commits(prev_tag, current_tag)
 
+          git_commits = @git.log.between(prev_tag, current_tag)
+
+          mapped_commits = git_commits.map do |gc|
+
+            c = Commit.new
+            c.author = gc.author
+            c.message = gc.message
+            c.name = gc.name
+            c.committed_on = gc.date
+
+            c
+
+          end
+
+          mapped_commits
+
+        end
+
+        def tag_exists(tag_name)
+          exists = false
+          begin
+            @git.tag(tag_name)
+            exists = true
+          rescue Git::GitTagNameDoesNotExist => tag_error
+
+          end
+
+          exists
+
+        end
+
+        def get_story_ids(commits, story_id_regex_pattern = '^\[?([A-Z]{2,8}-\d{1,11})')
+
+          ids = []
+          re = Regexp.new(story_id_regex_pattern)
+
+          commits.each do |commit|
+            re.match(commit.message) do |m|
+              ids << m[1] if m.length > 0
+            end
+          end
+
+          ids.uniq
+
+        end
+
+
+      end
+
+      class Commit
+        attr_accessor :message
+        attr_accessor :name
+        attr_accessor :parent
+        attr_accessor :author
+        attr_accessor :committed_on
       end
 
 
     end
-
-    class Commit
-      attr_accessor :message
-      attr_accessor :name
-      attr_accessor :parent
-      attr_accessor :author
-      attr_accessor :committed_on
-    end
-
-
   end
 end
